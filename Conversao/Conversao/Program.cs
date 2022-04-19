5eea7928cc64d8f37c2290751302ec6fd23cf0e6
Kaciany Ramos <70922914+Kaciany@users.noreply.github.com>
2022-04-19 09:35:04 -0300

    Conversão implícita de tipos numéricos

---
+++ Conversao/Conversao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            // Converção implícita  de tipos numéricos
+
+            // Verificar se a abrangência de valores dos tipos de dados numéricos são compatíveis para que
+            // aconteça a conversão desses valores.
+
+            byte num1 = 100; // 8 bits, de 0 a 255
+            ushort num2;
+
+            num2 = num1;
+
+            float num3 = 1250.45f;
+            double num4 = num3;
+
+            int numero = 'C';
+
+            Console.WriteLine(numero);
+            Console.ReadKey();
+
+        }
+
+    }
+}
